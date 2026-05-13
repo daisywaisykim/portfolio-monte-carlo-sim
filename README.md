919157e2fe7cbb3a2f10ac6e34671ac1d9e7ace8
Daisy Kim <daisy@kortex.co>
2026-05-13 15:19:07 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Monte Carlo Simulation for Portfolio Optimization
+
+TODO: Description
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main(int, char**){
+    std::cout << "Hello, from portfolio-monte-carlo-sim!\n";
+}
